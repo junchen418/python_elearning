2b4cd80228c875c61769b9c0881dfbb9b011d282
xuchen <xuchen@patsnap.com>
2018-12-21 10:45:11 +0800

    学习函数的定义

--- Function/Function_1.py
+++ Function/Function_1.py
@@ -1,2 +1,12 @@
 __author__ = "Damon"
 #函数的定义   def 函数名 （）  位置参数  默认参数   位置参数必须在默认参数之前显示
+#可变参数的使用
+#关键字参数的使用
+
+def changeParams(*args):
+    for i in args:
+        print(i)
+
+if __name__ == '__main__':
+    lsit_1 = [1,2,3,4,5,56]
+    changeParams(*lsit_1)
